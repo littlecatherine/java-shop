25e2b283558b44abe4949b0bae0b279fe162ba5f
Joe <rxyjoey@gmail.com>
2015-11-15 03:30:20 +0000

    Add FoodService and Implment

---
+++ src/main/java/com/joecui/web/service/FoodService.java
@@ -0,0 +1,17 @@
+package com.joecui.web.service;
+
+import com.joecui.web.entity.Food;
+
+import java.util.List;
+
+public interface FoodService {
+
+//	User findById(Integer id);
+	
+	List<Food> findAll();
+
+//	void saveOrUpdate(User user);
+//
+//	void delete(int id);
+	
+}
